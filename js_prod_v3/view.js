7274751450f1a7799f03f65ef25fb2f64b6fcb44
顾振(技术中心) <zhengu@sohu-inc.com>
2014-08-20 20:12:57 +0800

    s

--- js_prod_v3/view.js
+++ js_prod_v3/view.js
@@ -153,7 +153,7 @@ function showText(text) {
         var tempid = Date.now();
         $.post("http://effyroth.com/rank?appname=hextris&uid=" + tempid + "&score=" + score, function(data){
             data = JSON.parse(data)
-            dp_submitScore(score, data.user.rank);
+            dp_submitScore(score, data.user);
         });
         
     }
